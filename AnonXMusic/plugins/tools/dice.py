52f2a357231c52708a1d3ecef502d8b4d874c859
parthxsharma <151134722+parthxsharma@users.noreply.github.com>
2023-12-23 17:25:54 +0530

    Update dice.py

--- AnonXMusic/plugins/tools/dice.py
+++ AnonXMusic/plugins/tools/dice.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, enums, filters
 #from config import *
 import asyncio
-from Anonx import app as app
+from AnonX import app as app
 
 from pyrogram.handlers import MessageHandler
 
